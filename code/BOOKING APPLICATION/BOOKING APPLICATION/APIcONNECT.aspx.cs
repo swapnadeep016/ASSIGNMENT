550eefba9bf3e9d0b4e0b48c8c41eba417492d9f
swapnadeep016 <40471771+swapnadeep016@users.noreply.github.com>
2019-07-03 17:53:53 +0100

    Sample API
    Yesterday I was studying about API. Today I have practiced on the blank page to check that it is working properly or not.

---
+++ code/BOOKING APPLICATION/BOOKING APPLICATION/APIcONNECT.aspx.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace BOOKING_APPLICATION
+{
+    public partial class APIcONNECT : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            WebClient client = new WebClient();
+           
+            string resp = client.DownloadString("http://api.apixu.com/v1/current.json?key=d220789591ae4645a39161925190307&q=Paris");
+            dynamic obj = JsonConvert.DeserializeObject<dynamic>(resp);
+            string temp = obj["current"]["temp_c"].ToString();
+            Label1.Text = temp;
+
+        }
+    }
+}
